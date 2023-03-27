d8e6270bf66e401e1be69927df96230d73a5cb8b
NadezdaIS <nadezhdaivanova29@gmail.com>
2023-03-27 18:25:58 +0300

    Update

--- src/main/java/phonebook/PhonebookController.java
+++ src/main/java/phonebook/PhonebookController.java
@@ -2,13 +2,24 @@ package phonebook;
 
 import java.util.Scanner;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PhonebookController {
-    private Scanner scanner = new Scanner(System.in);
-    private StorageManager storageManager = new StorageManager();
+    private final Scanner scanner = new Scanner(System.in);
+    private final StorageManager storageManager;
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public PhonebookController (){
+        this.storageManager = new StorageManager();
+        this.syncContacts();
+    }
 
     public void collectAndSaveContact() {
         try {
             this.storageManager.addContact(this.collectContactInfo());
+            this.syncContacts();
             System.out.println("Contact Added successfully");
         } catch (Exception exception){
             exception.printStackTrace();
@@ -27,6 +38,27 @@ public class PhonebookController {
         return contact;
     }
 
+    public void showAllContacts(ArrayList<Contact> contacts){
+        contacts = contacts == null ? this.contacts : contacts;
+        System.out.println("SN \tName \t Number");
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts to display");
+            return;
+        }
+
+        int SN = 1;
+
+        for (Contact contact: contacts){
+            System.out.println(
+                    SN + "\t" +
+                            contact.getFirstName() + " " +
+                            contact.getLastName() + "\t" +
+                            contact.getPhoneNumber() + "\t"
+            );
+
+            SN ++;
+        }
+    }
     public void displayMenu(){
         System.out.println("Welcome to Phonebook!" +
                 "\n1. Add Contact" +
@@ -38,5 +70,58 @@ public class PhonebookController {
     }
 
 
+    public void syncContacts() {
+        try {
+            this.contacts = this.storageManager.getContacts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeContact() {
+        this.showAllContacts(null);
+        System.out.println("Enter the contact SN to remove:");
+        try {
+            int contactIndex = Integer.parseInt(scanner.nextLine());
+            this.contacts.remove(contactIndex - 1);
+            this.storageManager.overwriteContacts(this.contacts);
+            System.out.println("Contact deleted successfully");
+        } catch (Exception exception){
+            System.out.println("Problem deleting contact: " + exception.getClass() + exception.getMessage());
+        }
+    }
+
+    public void updateContact() {
+        try {
+            this.showAllContacts(null);
+            System.out.println("Enter the contact SN to update:");
+            int contactIndex = Integer.parseInt(scanner.nextLine());
+            Contact updatedContactInfo = this.collectContactInfo();
+            this.contacts.set(contactIndex - 1, updatedContactInfo);
+            this.storageManager.overwriteContacts(this.contacts);
+            this.syncContacts();
+            System.out.println("Contact updated successfully");
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void findContact() {
+        System.out.println("Enter the name or number to find:");
+        String contactToFind = scanner.nextLine();
+
+        ArrayList<Contact> foundContacts = new ArrayList<>();
+
+        for (Contact contact: this.contacts){
+            if (contact.toString().toLowerCase().trim().contains(contactToFind.trim().toLowerCase())){
+                foundContacts.add(contact);
+            }
+        }
+
+        System.out.println(foundContacts.size() + " contacts found");
+
+        this.showAllContacts(foundContacts);
+    }
 }
+
 
